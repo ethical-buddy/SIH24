ac9b6c83fa44ae532a7882e84189835b8829223e
virtualISP <146754218+virtualISP@users.noreply.github.com>
2024-09-02 14:16:47 +0530

    5 new benchmarks

--- suryansh_d/code/cis-benchmark-audit-tool/internal/benchmarks/linux.go
+++ suryansh_d/code/cis-benchmark-audit-tool/internal/benchmarks/linux.go
@@ -1,10 +1,12 @@
 package benchmarks
 
 import (
+	"fmt"
 	"os/exec"
 	"sync"
-       )
+)
 
+// CheckLinuxFirewall checks the status of the firewall
 func CheckLinuxFirewall() (string, error) {
 	cmd := exec.Command("ufw", "status")
 	output, err := cmd.CombinedOutput()
@@ -14,12 +16,83 @@ func CheckLinuxFirewall() (string, error) {
 	return string(output), nil
 }
 
+// DisableCramfs disables the cramfs module
+func DisableCramfs() (string, error) {
+	denyCmd := exec.Command("sh", "-c", `echo "install cramfs /bin/false" >> /etc/modprobe.d/cramfs.conf && echo "blacklist cramfs" >> /etc/modprobe.d/cramfs.conf`)
+	if err := denyCmd.Run(); err != nil {
+		return "", fmt.Errorf("Failed to deny list cramfs: %v", err)
+	}
+	unloadCmd := exec.Command("sh", "-c", "modprobe -r cramfs")
+	if err := unloadCmd.Run(); err != nil {
+		return "", fmt.Errorf("Failed to unload cramfs: %v", err)
+	}
+	return "cramfs module disabled successfully.", nil
+}
+
+// DisableFreevxfs disables the freevxfs module
+func DisableFreevxfs() (string, error) {
+	denyCmd := exec.Command("sh", "-c", `echo "install freevxfs /bin/false" >> /etc/modprobe.d/freevxfs.conf && echo "blacklist freevxfs" >> /etc/modprobe.d/freevxfs.conf`)
+	if err := denyCmd.Run(); err != nil {
+		return "", fmt.Errorf("Failed to deny list freevxfs: %v", err)
+	}
+	unloadCmd := exec.Command("sh", "-c", "modprobe -r freevxfs")
+	if err := unloadCmd.Run(); err != nil {
+		return "", fmt.Errorf("Failed to unload freevxfs: %v", err)
+	}
+	return "freevxfs module disabled successfully.", nil
+}
+
+// DisableJffs2 disables the jffs2 module
+func DisableJffs2() (string, error) {
+	denyCmd := exec.Command("sh", "-c", `echo "install jffs2 /bin/false" >> /etc/modprobe.d/jffs2.conf && echo "blacklist jffs2" >> /etc/modprobe.d/jffs2.conf`)
+	if err := denyCmd.Run(); err != nil {
+		return "", fmt.Errorf("Failed to deny list jffs2: %v", err)
+	}
+	unloadCmd := exec.Command("sh", "-c", "modprobe -r jffs2")
+	if err := unloadCmd.Run(); err != nil {
+		return "", fmt.Errorf("Failed to unload jffs2: %v", err)
+	}
+	return "jffs2 module disabled successfully.", nil
+}
+
+// DisableHfs disables the hfs module
+func DisableHfs() (string, error) {
+	denyCmd := exec.Command("sh", "-c", `echo "install hfs /bin/false" >> /etc/modprobe.d/hfs.conf && echo "blacklist hfs" >> /etc/modprobe.d/hfs.conf`)
+	if err := denyCmd.Run(); err != nil {
+		return "", fmt.Errorf("Failed to deny list hfs: %v", err)
+	}
+	unloadCmd := exec.Command("sh", "-c", "modprobe -r hfs")
+	if err := unloadCmd.Run(); err != nil {
+		return "", fmt.Errorf("Failed to unload hfs: %v", err)
+	}
+	return "hfs module disabled successfully.", nil
+}
+
+// DisableHfsplus disables the hfsplus module
+func DisableHfsplus() (string, error) {
+	denyCmd := exec.Command("sh", "-c", `echo "install hfsplus /bin/false" >> /etc/modprobe.d/hfsplus.conf && echo "blacklist hfsplus" >> /etc/modprobe.d/hfsplus.conf`)
+	if err := denyCmd.Run(); err != nil {
+		return "", fmt.Errorf("Failed to deny list hfsplus: %v", err)
+	}
+	unloadCmd := exec.Command("sh", "-c", "modprobe -r hfsplus")
+	if err := unloadCmd.Run(); err != nil {
+		return "", fmt.Errorf("Failed to unload hfsplus: %v", err)
+	}
+	return "hfsplus module disabled successfully.", nil
+}
+
+// RunLinuxChecks runs all the defined checks
 func RunLinuxChecks() string {
 	var wg sync.WaitGroup
 	results := ""
 
 	checks := []func() (string, error){
 		CheckLinuxFirewall,
+		DisableCramfs,
+		DisableFreevxfs,
+		DisableJffs2,
+		DisableHfs,
+		DisableHfsplus,
 		// Add more Linux check functions here
 	}
 
@@ -39,4 +112,3 @@ func RunLinuxChecks() string {
 
 	return results
 }
-
